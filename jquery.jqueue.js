28a46477b75733390ea067ea0ab270a35bbefc1d
Jared Hall <jrhall@gmail.com>
2014-03-08 09:34:29 -0700

    initial commit of jQueue

---
+++ jquery.jqueue.js
@@ -0,0 +1,61 @@
+(function($){
+    $.fn.jQueue = {
+
+        ids: {},
+
+        objects: {},
+
+        keys: {},
+
+        callbacks: {},
+
+        args: {},
+
+        waiting: false,
+
+        wait: function() {
+            $.fn.jQueue.waiting = false;
+            $.each($.fn.jQueue.ids, function(index, id){
+                $.fn.jQueue.check(
+                    $.fn.jQueue.objects[id], $.fn.jQueue.keys[id],
+                    $.fn.jQueue.callbacks[id], $.fn.jQueue.args[id],
+                    $.fn.jQueue.ids[id]);
+                $.fn.jQueue.waiting = true;
+            });
+            if($.fn.jQueue.waiting === true)
+                window.setTimeout($.fn.jQueue.wait, 250);
+        },
+
+        guid: function() {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+                return v.toString(16);
+            });
+        },
+
+        init: function(object, key, callback, args) {
+            var id = this.guid();
+            this.ids[id] = id;
+            this.objects[id] = object;
+            this.keys[id] = key;
+            this.callbacks[id] = callback;
+            this.args[id] = args;
+            if(!this.check(object, key, callback, args, id))
+                this.wait();
+        },
+
+        check: function(object, key, callback, args, id) {
+            if(typeof(object[key]) !== "undefined") {
+                callback(args);
+                delete this.objects[id];
+                delete this.ids[id];
+                delete this.callbacks[id];
+                delete this.keys[id];
+                delete this.args[id];
+                return true;
+            }
+            return false;
+        }
+
+    };
+})(jQuery);
